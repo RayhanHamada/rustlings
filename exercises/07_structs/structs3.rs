04e144a10ffccdb659d0ce7b97213318fecdbca5
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2023-12-18 15:50:41 +0700

    till error 1

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::ops::Mul;
 
 #[derive(Debug)]
 struct Package {
@@ -31,12 +31,12 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.recipient_country.ne(&self.sender_country)
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
+        self.weight_in_grams.mul(cents_per_gram)
     }
 }
 
--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -14,14 +14,31 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+impl Team {
+    fn new(scored: u8, conceded: u8) -> Self {
+        Self {
+            goals_scored: scored,
+            goals_conceded: conceded,
+        }
+    }
+}
+
+impl std::default::Default for Team {
+    fn default() -> Self {
+        Self {
+            goals_scored: 0,
+            goals_conceded: 0,
+        }
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,7 +56,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        scores.entry(team_1_name.clone()).or_default();
+        scores.entry(team_2_name.clone()).or_default();
+
+        scores.entry(team_1_name).and_modify(|e| {
+            e.goals_scored += team_1_score;
+            e.goals_conceded += team_2_score;
+        });
+
+        scores.entry(team_2_name).and_modify(|e| {
+            e.goals_scored += team_2_score;
+            e.goals_conceded += team_1_score;
+        });
     }
+
     scores
 }
 
